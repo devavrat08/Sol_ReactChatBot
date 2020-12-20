b4c381af82e5c423211e96b11490ebc2d0b60040
devavrat08 <devshahasane@gmail.com>
2020-12-20 13:19:18 +0530

    chatbot done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Chatbot from 'react-simple-chatbot';
+import { Segment } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(){
+
+    const steps=[
+        {
+            id:'Greet',
+            message:'Hello, Welcome to our website',
+            trigger:'Ask Name'
+        },
+        {
+            id:'Ask Name',
+            message:'Please enter your name',
+            trigger:'waiting1'
+        },
+        {
+            id:'waiting1',
+            user:true,
+            trigger:'Name'
+        },
+        {
+            id:'Name',
+            message:'Hi {previousValue},Please select your issue',
+            trigger:'issues'
+        },
+        {
+            id:'issues',
+            options:[
+            {
+                    value:'React',
+                    label:'React',
+                    trigger:'React'
+            },
+            {
+                value:'Angular',
+                label:'Angular',
+                trigger:'Angular'
+            }
+            ],
+        },
+            {
+                id:'React',
+                message:'Thanks for telling your React issue',
+                end:true
+            },
+            {
+                id:'Angular',
+                message:'Thanks for telling your Angular issue',
+                end:true
+            }
+            
+        
+    ]
+    return <>
+    <Segment floated="right">
+<Chatbot steps={steps}/>
+    </Segment>
+    </>
 }
+
+
 
 export default App;
